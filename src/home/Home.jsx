dc90f1e4a41637538646cb0dcadc8011c8340adf
KiwiScreams <screamskiwi3@gmail.com>
2024-09-09 16:34:08 +0400

    props

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,20 @@
 import Product from "../product/Product";
 import "./Home.css";
+import data from "../data";
 const Home = () => {
   return (
     <>
       <h1>home</h1>
-      <Product name="name" desc="desc"/>
+      {data.products.map((prod, index) => {
+        return (
+          <Product
+            image={prod.image}
+            name={prod.name}
+            desc={prod.desc}
+            price={prod.price}
+          />
+        );
+      })}
     </>
   );
 };
--- src/product/Product.jsx
+++ src/product/Product.jsx
@@ -6,6 +6,8 @@ const Product = (props) => {
         <h1>product</h1>
         <div>
           <h2>{props.name}</h2>
+          <h3>{props.price}</h3>
+          <img src={props.image} alt="" />
           <p>{props.desc}</p>
         </div>
       </section>
