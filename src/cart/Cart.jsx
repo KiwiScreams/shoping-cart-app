4811ee30ea8b36f710bbc5194b479f3f0950bd11
KiwiScreams <screamskiwi3@gmail.com>
2024-09-09 17:47:45 +0400

    empty cart text

--- src/cart/Cart.jsx
+++ src/cart/Cart.jsx
@@ -1,8 +1,22 @@
 import "./Cart.css";
+import { useCart } from "react-use-cart";
 const Cart = () => {
-    return ( 
-        <></>
-     );
-}
- 
+  const { 
+    isEmpty,
+    totalUniqueItems,
+    items,
+    totalItems,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart
+   } = useCart();
+   if(isEmpty) return <h1>your cart is empty</h1>
+  return (
+  <>
+
+  </>
+  );
+};
+
 export default Cart;
